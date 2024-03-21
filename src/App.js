4f5831e244d7f021d7e47be750fa6021cb000579
a-gehlot <andrew.gehlot@gmail.com>
2024-03-21 01:42:40 -0400

    Switch routes with not found page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
 import GalleryView from "./components/GalleryView";
-import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <div className="page-wrapper">
       <GalleryNavigation galleries={harvardArt}/>
-      <Route path='/galleries/:galleryId'>
-        <GalleryView galleries={harvardArt} />
-      </Route>
+      <Switch>
+        <Route exact path='/'>
+          <h2>Harvard Art Museum</h2>
+          <p>
+            "Look, but Don't Touch. Please select a Gallery in the navigation bar."
+          </p>
+        </Route>
+        <Route path='/galleries/:galleryId'>
+          <GalleryView galleries={harvardArt} />
+        </Route>
+        <Route>
+          <h2>Page not found!</h2>
+        </Route>
+      </Switch>
+      
     </div>
   );
 }
